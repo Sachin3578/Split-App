2e63f80904c51cdb15243c0f04f7af2c312358a7
sachin gornar <gornarsachin2004@gmail.com>
2025-06-18 08:18:43 +0530

    Fix: use process.env.PORT for Render deployment

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ require('dotenv').config();
 const expenseRoutes = require('./routes/expenses');
 const settlementRoutes = require('./routes/settlements');
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 const app = express(); 
 
 app.use(cors());
