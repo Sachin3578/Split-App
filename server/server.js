8aff83fba954012b8e23755f493e67984657dcd9
sachin gornar <gornarsachin2004@gmail.com>
2025-06-18 08:34:26 +0530

    server runninng on port resolve

--- server/server.js
+++ server/server.js
@@ -8,11 +8,9 @@ require('dotenv').config();
 const expenseRoutes = require('./routes/expenses');
 const settlementRoutes = require('./routes/settlements');
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+
 
 const app = express(); 
 
@@ -23,10 +21,11 @@ app.use(express.json());
 app.use('/expenses', expenseRoutes);
 app.use('/', settlementRoutes);
 
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(5000, () => console.log("Server started on port 5000"));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch(err => console.log("MongoDB connection error:", err));
