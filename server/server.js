e717bf8433bf7485bd12ff23331459a70cc14a52
sachin gornar <gornarsachin2004@gmail.com>
2025-06-18 08:31:09 +0530

    port error solve

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const expenseRoutes = require('./routes/expenses');
 const settlementRoutes = require('./routes/settlements');
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
